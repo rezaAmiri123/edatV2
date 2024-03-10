8bc6f235a1bdfe8071cf40082d6e07e7c2946650
Reza Amiri <reza.amiri.ac@gmail.com>
2024-03-10 11:40:27 +0300

    improve postgres

--- postgres/inbox_store.go
+++ postgres/inbox_store.go
@@ -5,9 +5,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-
-	_"github.com/jackc/pgx/v5"
+	
+	"github.com/jackc/pgconn"
 	"github.com/jackc/pgerrcode"
+	// "github.com/jackc/pgx/v5/pgconn"
 	"github.com/rezaAmiri123/edatV2/am"
 	"github.com/rezaAmiri123/edatV2/tm"
 )
