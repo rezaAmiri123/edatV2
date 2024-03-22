e687758730eeeee2fec0d032d795171ab6fb532b
Reza Amiri <reza.amiri.ac@gmail.com>
2024-03-22 10:52:54 +0300

    add grpc

---
+++ http/context.go
@@ -0,0 +1,46 @@
+package edathttp
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/rezaAmiri123/edatV2/core"
+)
+
+// Request tracking
+const (
+	RequestIDHeader     = "X-Request-Id"
+	CorrelationIDHeader = "X-Correlation-Id"
+	CausationIDHeader   = "X-Causation-Id"
+)
+
+// RequestContext is an http.Handler middleware that sets the id, correlation, and causation ids into context
+func RequestContext(next http.Handler)http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestID := r.Header.Get(RequestIDHeader)
+		if requestID == ""{
+			requestID = uuid.New().String()
+		}
+		correlationID := r.Header.Get(CorrelationIDHeader)
+		if correlationID == "" {
+			correlationID = requestID
+		}
+		causationID := r.Header.Get(CausationIDHeader)
+		if causationID == "" {
+			causationID = requestID
+		}
+
+		ctx := core.SetRequestContext(r.Context(), requestID, correlationID, causationID)
+
+		w.Header().Set(RequestIDHeader, core.GetRequestID(ctx))
+		next.ServeHTTP(w,r.WithContext(ctx))
+	})
+}
+
+// SetResponseHeaders puts the id, correlation, and causation ids into the outgoing http response
+func SetResponseHeaders(ctx context.Context, w http.ResponseWriter){
+	w.Header().Set(RequestIDHeader, core.GetRequestID(ctx))
+	w.Header().Set(CorrelationIDHeader, core.GetCorrelationID(ctx))
+	w.Header().Set(CausationIDHeader, core.GetCausationID(ctx))
+}
---
+++ http/error_response.go
@@ -0,0 +1,10 @@
+package edathttp
+
+import(
+	"github.com/go-chi/render"
+	"github.com/stackus/errors"
+
+)
+func NewErrorResponse(err error)render.Renderer{
+	
+}
